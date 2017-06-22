64faf95f7d00a92476000f0c3c3a891a2978dffd
Leonardo Rico <leonardo.ricog@gmail.com>
2017-06-22 14:37:30 -0500

    update HorizontalCard responsive

--- src/components/molecules/FileCardHorizontal/index.js
+++ src/components/molecules/FileCardHorizontal/index.js
@@ -25,13 +25,36 @@ const Wrapper = styled.div `
     }
   }
   .options{
-    padding: 0px 20px 0px 0px;
+    padding: 15px 20px 20px 20px;
+
   }
 
+
 .card-title{
-  font-size: ${font('xxs')} !important;
-	color: ${palette('basescale', 2)} !important;
-  font-weight: 500;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-style: italic;
+}
+.card-subtitle{
+  font-size: 12px !important;
+}
+.card-right{
+  width: 100% !important;
+  padding: 10px !important;
+}
+.card-footer{
+  margin: 0 !important;
+  font-size: 13px !important;
+  background:  ${palette('basescale', 10)} !important;;
+  padding: 0 12px !important;
+  height: 30px !important;
+  line-height: 30px !important;
+  color:white !important;
+  border-radius: 0px !important;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
 }
 
 `
@@ -42,25 +65,34 @@ class FileCardHorizontal extends React.Component {
     super(props);
   }
 
+  title(text){
+    return text.split(' ').slice(0,2).join(' ');
+  }
+  subtitle(text){
+    return text.split(' ').slice(2).join(' ');
+  }
+
   render() {
     return (
       <Wrapper>
         <Link to={`/file/summary/${this.props.record.id}`}>
           <Paper>
+
             <Row>
               <Col xs={12} sm={4} md={4} lg={4} className="img-media">
                 <img src={`https://s3.amazonaws.com/sib-resources/images/catalogo/miniatura/png/t-anfibios.png`}/>
               </Col>
-              <Col xs={12} sm={8} md={8} lg={8} className="paper-padding-1">
+              <Col xs={12} sm={8} md={8} lg={8} className="card-right">
                 <div className="options">
-                  <TitleSection className="padding-bottom-1 card-title">{this.props.record.name}</TitleSection>
-                  <FlatButton className="btn-option align-left" fullWidth={true}>
-                    <Comment/> {this.props.record.comment}
-                  </FlatButton>
+                  <FileStatus title="EN"/>
+                  <TitleSection className="padding-bottom-1 card-title">{this.title(this.props.record.scientificNameSimple)}</TitleSection>
+                  <TitleSection className="card-subtitle">{this.subtitle(this.props.record.scientificNameSimple)}</TitleSection>
                   <p className="align-justify">
                     Lorem ipsum dolor sit amet, consectetur piscing elipiscing elipiscing eliadipiscing elit. Morbi luctus leo non sapien tempus, ac cursus lectus tristique. Donec vitae mattis ante. Donec vitae mattis ante
                   </p>
-                  <FileStatus />
+                  <div className="align-left padding card-footer">
+                    {this.props.record.creation_date}
+                  </div>
                 </div>
               </Col>
             </Row>
