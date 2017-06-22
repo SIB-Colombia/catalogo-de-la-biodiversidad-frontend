cc448feec5720daeae5623983153c81b1a0f5400
Leonardo Rico <leonardo.ricog@gmail.com>
2017-06-22 16:29:05 -0500

    Added Component FileDetailMenu

--- src/components/organisms/FileDetailMenu/index.js
+++ src/components/organisms/FileDetailMenu/index.js
@@ -7,12 +7,16 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 import Menu from 'material-ui/svg-icons/navigation/menu';
 import ArrowBack from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
 import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
 import Chip from 'material-ui/Chip';
 import {List, ListItem} from 'material-ui/List';
 import ActionGrade from 'material-ui/svg-icons/action/grade';
 import Subheader from 'material-ui/Subheader';
 import {Link} from 'components';
 import {size, palette} from 'styled-theme';
+import Scrollchor from 'react-scrollchor';
+
 
 const Wrapper = styled.div `
 
@@ -29,14 +33,15 @@ width: 35px;
       width: 30px !important;
   }
 }
-.drawer{
-  margin-top: 65px;
+.index{
+  color: #444;
+  font-weight: 600;
+  font-size: 25px;
+  text-align: center;
+  padding: 15px 5px 15px 5px;
+  border-bottom: 1px solid #e8e8e8;
 }
-.btnFilters{
-    /*position: fixed;
-    left: 0px;
-    top: 0px;*/
-}
+
 .style-btn{
   box-shadow: none !important;
 
@@ -57,131 +62,27 @@ class FileDetailMenu extends React.Component {
     super(props)
     this.state = {
       open: false,
-      schema: [
-        {
-          'category': 'Grupos Biológicos',
-          'items': [
-            {
-              name: 'Anfibios',
-              value: 1634
-            }, {
-              name: 'Aves',
-              value: 2755
-            }, {
-              name: 'Hongos',
-              value: 3345
-            }, {
-              name: 'Insectos',
-              value: 4355
-            }, {
-              name: 'Mamíferos',
-              value: 5674
-            }, {
-              name: 'Peces',
-              value: 6325
-            }, {
-              name: 'Plantas',
-              value: 73577
-            }, {
-              name: 'Reptiles',
-              value: 8455
-            }
-          ]
-        }, {
-          'category': 'Departamentos',
-          'items': [
-            {
-              name: 'Cundinamarca',
-              value: 1832
-            }, {
-              name: 'Boyacá',
-              value: 2124
-            }
-          ]
-        }, {
-          'category': 'Ecosistemas',
-          'items': [
-            {
-              name: 'Bosque tropical',
-              value: 135565
-            }, {
-              name: 'Desierto',
-              value: 2456
-            }, {
-              name: 'Bosque seco',
-              value: 33466
-            }, {
-              name: 'Páramo',
-              value: 46225
-            }, {
-              name: 'Bosque de niebla',
-              value: 51123
-            }, {
-              name: 'Humedales',
-              value: 665433
-            }
-          ]
-        }
-      ],
-      chipData: []
-    };
-
-    this.styles = {
-      chip: {
-        margin: 4
-      },
-      wrapper: {
-        display: 'flex',
-        flexWrap: 'wrap'
-      }
-    };
+      sections: []
+    }
   }
 
-  handleRequestDelete = (data) => {
-    // console.log(data);
-    //Chip
-    this.chipData = this.state.chipData;
-    const chipToDelete = this.chipData.map((chip) => chip.value).indexOf(data.value);
-    this.chipData.splice(chipToDelete, 1);
-    this.setState({chipData: this.chipData});
-    //schema update
-    this.schema = this.state.schema;
-    this.schema[data.i].items.unshift(data);
-    this.setState({schema: this.schema});
-  };
+  componentWillMount() {
 
-  handleAddChip = (record, i, j) => {
-    //Chip
-    this.chipData = this.state.chipData;
-    record.i = i;
-    record.j = j;
-    this.chipData.push(record);
-    this.setState({chipData: this.chipData});
-    //schema update
-    this.schema = this.state.schema;
-    this.schema[i].items.splice(j, 1);
-    this.setState({schema: this.schema});
-  };
+    let sections = [];
+    for (var property in this.props.complete) {
+      if(property.indexOf('InUse') > -1){
+        sections[property] = true;
+      }
+    }
+    this.setState({sections :sections});
+  }
 
   handleToggleMenu = () => this.setState({
     open: !this.state.open
   });
 
-  componentDidMount() {}
-
-  renderChip(data) {
-
-    return (
-      <Chip className="animated fadeInUp" key={data.value} onRequestDelete={() => this.handleRequestDelete(data)} style={this.styles.chip}>
-        {data.name}
-      </Chip>
-    );
-  }
 
   render() {
-
-    const subcat = this.state.schema.map((record, i) => record.items.map((subrecord, j) => <ListItem key={j} primaryText={subrecord.name} onTouchTap={() => this.handleAddChip(subrecord, i, j)} leftIcon={< ActionGrade />}/>));
-    const listItems = this.state.schema.map((record, i) => <ListItem key={i} nestedItems={subcat[i]} primaryText={record.category} initiallyOpen={true} primaryTogglesNestedList={true}/>);
 
     return (
       <Wrapper>
@@ -191,13 +92,10 @@ class FileDetailMenu extends React.Component {
           </FloatingActionButton>
         </div>
         <Drawer open={this.state.open} containerClassName="drawer">
-          <List>
-            <Subheader>Filtros activos</Subheader>
-            <div style={this.styles.wrapper}>
-              {this.state.chipData.map(this.renderChip, this)}
-            </div>
-            {listItems}
-          </List>
+            <div className="index">Índice de ficha</div>
+            {Object.keys(this.state.sections).map((section,i) => (
+              <Scrollchor key={i} to={`#${section}`} animate={{offset: -80, duration: 600}}  className="nav-link"><MenuItem>{section}</MenuItem></Scrollchor>
+            ))}
         </Drawer>
       </Wrapper>
     )
