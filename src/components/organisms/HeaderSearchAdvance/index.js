70de5d8ce97caea484e3e0d1aad2305acb7b8e99
Jorge González <jag2kn@gmail.com>
2017-06-23 20:43:01 -0500

    Add advanced search items #175

--- src/components/organisms/HeaderSearchAdvance/index.js
+++ src/components/organisms/HeaderSearchAdvance/index.js
@@ -46,89 +46,126 @@ class HeaderSearchAdvance extends React.Component {
 
     return (
       <Wrapper>
+        <Row>
+          <Col xs={12} sm={3} md={3} lg={3}>
+            <Subheader><strong>Taxonomía</strong></Subheader>
+            
+            <Subheader>Reino</Subheader>
+            <Checkbox label="Reino"/>
+            <Checkbox label="Animales"/>
+            <Checkbox label="Plantas"/>
+            <Checkbox label="Hongos"/>
+            <Checkbox label="Bacterias"/>
+            <Checkbox label="Arquea"/>
 
-        <Row className="box-search">
-          <Col xs={12} sm={12} md={12} lg={2}>
-            <SelectField fullWidth={true} value={this.state.files} onChange={this.handleChange} maxHeight={200}>
-              {items}
-            </SelectField>
+            <Subheader>Grupos de Animales</Subheader>
+            <Checkbox label="Mamíferos"/>
+            <Checkbox label="Aves"/>
+            <Checkbox label="Reptiles"/>
+            <Checkbox label="Anfibios"/>
+            <Checkbox label="Peces"/>
+            <Checkbox label="Insectos"/>
+            <Checkbox label="Escarabajos"/>
+            <Checkbox label="Mariposas"/>
+            <Checkbox label="Hormigas"/>
+            <Checkbox label="Abejas"/>
+            <Checkbox label="Moscas y zancudos"/>
+            <Checkbox label="Arácnidos"/>
+            <Checkbox label="Moluscos"/>
+            <Checkbox label="Decápodos"/>
+            <Checkbox label="Equinodermos"/>
+            <Checkbox label="Esponjas"/>
+            <Checkbox label="Corales y afines"/>
+           
           </Col>
-          <Col xs={12} sm={12} md={12} lg={10}>
-            <TextField hintText="Buscar..." fullWidth={true}/>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} sm={6} md={3} lg={3}>
-            <SelectField fullWidth={true} floatingLabelText="Grupo biológico" value={this.state.biologicalGroup} onChange={this.handleChange}>
-              <MenuItem value={1} primaryText="Opción 1"/>
-              <MenuItem value={2} primaryText="Opción 2"/>
-            </SelectField>
-          </Col>
-          <Col xs={12} sm={6} md={3} lg={3}>
-            <SelectField fullWidth={true} floatingLabelText="Departamentos" value={this.state.departments} onChange={this.handleChange}>
-              <MenuItem value={1} primaryText="Opción 1"/>
-              <MenuItem value={2} primaryText="Opción 2"/>
-            </SelectField>
-          </Col>
-          <Col xs={12} sm={6} md={3} lg={3}>
-            <SelectField fullWidth={true} floatingLabelText="Ecosistema" value={this.state.ecosystem} onChange={this.handleChange}>
-              <MenuItem value={1} primaryText="Opción 1"/>
-              <MenuItem value={2} primaryText="Opción 2"/>
-            </SelectField>
-          </Col>
-          <Col xs={12} sm={6} md={3} lg={3}>
-            <SelectField fullWidth={true} floatingLabelText="Estado de amenaza" value={this.state.stateThreat} onChange={this.handleChange}>
-              <MenuItem value={1} primaryText="Opción 1"/>
-              <MenuItem value={2} primaryText="Opción 2"/>
-            </SelectField>
-          </Col>
-        </Row>
-        <br/>
-        <Row>
-          <Subheader>Mostrar</Subheader>
-          <Col xs={12} sm={6} md={6} lg={6}>
-            <Checkbox label="Salvaje"/>
-            <Checkbox label="En cautividad"/>
-            <Checkbox label="Verificable"/>
-            <Checkbox label="Grado de investigación"/>
-            <Checkbox label="Necesita identificador"/>
-          </Col>
-          <Col xs={12} sm={6} md={6} lg={6}>
-            <Checkbox label="Amenazado"/>
-            <Checkbox label="Introducida"/>
-            <Checkbox label="Popular"/>
-            <Checkbox label="Tiene sonidos"/>
-            <Checkbox label="Tiene Fotos"/>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} sm={6} md={6} lg={6}>
-            <Subheader>Revisada</Subheader>
-            <RadioButtonGroup name="shipSpeed" defaultSelected="not_light">
-              <RadioButton value="indiferente" label="Cualquiera"/>
-              <RadioButton value="si" label="Sí"/>
-              <RadioButton value="no" label="No"/>
-            </RadioButtonGroup>
-            <br/>
-            <SelectField fullWidth={true} floatingLabelText="Rango" value={this.state.range} onChange={this.handleChange}>
-              <MenuItem value={1} primaryText="Opción 1"/>
-              <MenuItem value={2} primaryText="Opción 2"/>
-            </SelectField>
-            <SelectField fullWidth={true} floatingLabelText="Ordenado por" value={this.state.orderBy} onChange={this.handleChange}>
-              <MenuItem value={1} primaryText="Opción 1"/>
-              <MenuItem value={2} primaryText="Opción 2"/>
-            </SelectField>
-          </Col>
-          <Col xs={12} sm={6} md={6} lg={6}>
-            <TextField floatingLabelText="Persona" fullWidth={true}/>
-            <TextField floatingLabelText="Proyecto" fullWidth={true}/>
-            <DatePicker floatingLabelText="Fecha observación" mode="landscape" fullWidth={true}/>
-            <SelectField fullWidth={true} floatingLabelText="Licencia" value={this.state.license} onChange={this.handleChange}>
-              <MenuItem value={1} primaryText="Opción 1"/>
-              <MenuItem value={2} primaryText="Opción 2"/>
-            </SelectField>
-          </Col>
+          <Col xs={12} sm={3} md={3} lg={3}>
+            
+            <Subheader><strong>Taxonomía</strong></Subheader>
 
+            <Subheader>Grupos de Plantas</Subheader>
+            <Checkbox label="Orquídeas"/>
+            <Checkbox label="Magnolias y afines"/>
+            <Checkbox label="Palmas"/>
+            <Checkbox label="Frailejones"/>
+            <Checkbox label="Cactus"/>
+            <Checkbox label="Bromelias, labiadas y pasifloras"/>
+            <Checkbox label="Fanerógamas"/>
+            <Checkbox label="Helechos y afines"/>
+            <Checkbox label="Zamias"/>
+            <Checkbox label="Musgos y afines"/>
+            
+            <hr/>
+            <Subheader><strong>Multimedia</strong></Subheader>
+
+            <Checkbox label="Con Imágenes"/>
+            <Checkbox label="Sin imágenes"/>
+            <Checkbox label="Con videos"/>
+            <Checkbox label="Sin Videos"/>
+            <Checkbox label="Con sonidos"/>
+            <Checkbox label="Sin sonidos"/>
+          </Col>
+          <Col xs={12} sm={3} md={3} lg={3}>
+            <Subheader><strong>Departamento</strong></Subheader>
+            <Checkbox label="Amazonas"/>
+            <Checkbox label="Antioquia"/>
+            <Checkbox label="Arauca"/>
+            <Checkbox label="Archipiélago de San Andrés, Providencia y Santa Catalina"/>
+            <Checkbox label="Atlántico"/>
+            <Checkbox label="Bogotá, D.C."/>
+            <Checkbox label="Bolívar"/>
+            <Checkbox label="Boyacá"/>
+            <Checkbox label="Caldas"/>
+            <Checkbox label="Caquetá"/>
+            <Checkbox label="Casanare"/>
+            <Checkbox label="Cauca"/>
+            <Checkbox label="Cesar"/>
+            <Checkbox label="Chocó"/>
+            <Checkbox label="Córdoba"/>
+            <Checkbox label="Cundinamarca"/>
+            <Checkbox label="Guainía"/>
+            <Checkbox label="Guaviare"/>
+            <Checkbox label="Huila"/>
+            <Checkbox label="La Guajira"/>
+            <Checkbox label="Magdalena"/>
+            <Checkbox label="Meta"/>
+            <Checkbox label="Nariño"/>
+            <Checkbox label="Norte de Santander"/>
+            <Checkbox label="Putumayo"/>
+            <Checkbox label="Quindío"/>
+            <Checkbox label="Risaralda"/>
+            <Checkbox label="Santander"/>
+            <Checkbox label="Sucre"/>
+            <Checkbox label="Tolima"/>
+            <Checkbox label="Valle del Cauca"/>
+            <Checkbox label="Vaupés"/>
+            <Checkbox label="Vichada"/>
+          </Col>
+          
+          <Col xs={12} sm={3} md={3} lg={3}>
+            <Subheader><strong>Temática</strong></Subheader>
+            
+            <Subheader>Categorías de Amenaza Global</Subheader>
+            <Checkbox label="Especies Amenazadas"/>
+            <Checkbox label="No evaluado (NE)"/>
+            <Checkbox label="Datos insuficientes (DE)"/>
+            <Checkbox label="Preocupación menor (LC)"/>
+            <Checkbox label="Casi amenazado (NT)"/>
+            <Checkbox label="Vulnerable (VU)"/>
+            <Checkbox label="En peligro (EN)"/>
+            <Checkbox label="En peligro crítico (CR)"/>
+            <Checkbox label="Extinto en estado silvestre (EW)"/>
+            <Checkbox label="Extinto (EX)"/>
+           
+            <Subheader>Comercio Internacional</Subheader>
+            <Checkbox label="Especies reportadas en CITES"/>
+            <Checkbox label="Apéndice I"/>
+            <Checkbox label="Apéndice II"/>
+            <Checkbox label="Apéndice III"/>
+ 
+            <Subheader>Invasoras</Subheader>
+            <Checkbox label="Especies Invasoras"/>
+          </Col>
+          
         </Row>
       </Wrapper>
     )
