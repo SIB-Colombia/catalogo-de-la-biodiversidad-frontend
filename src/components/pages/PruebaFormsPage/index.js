0574ef02db37a80b7b5dd12148bffc3a8666dd73
Jorge Gonzalez <jag2kn@gmail.com>
2017-09-22 20:52:24 -0500

    Se limpian los campos null del pedido y se hace un alert con la versión almacenada

--- src/components/pages/PruebaFormsPage/index.js
+++ src/components/pages/PruebaFormsPage/index.js
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react';
 import {PageTemplate, Header, Footer} from 'components';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import _ from 'lodash'
 
 import * as FileService from '../../../services/FileService';
 
@@ -22,19 +23,47 @@ class PruebaFormsPage extends React.Component {
   componentWillMount() {
 
     FileService.getCampo('5661b2a2f3b30194336dc7a6', 'full_description', '1').then(data => {
-      console.log("El dato del get es: ", data);
       this.setState({campo: JSON.stringify(data)});
     }).catch(err => {
       console.log(err);
     })
   }
+  
+  pruneEmpty(obj) {
+    return function prune(current) {
+      _.forOwn(current, function (value, key) {
+        //console.log("Verificando ", key, value);
+        if (
+            _.isNull(value) ||
+            (_.isObject(value) && _.isEmpty(prune(value)))
+        )
+        /*
+          _.isUndefined(value) ||  || _.isNaN(value) ||
+          (_.isString(value) && _.isEmpty(value)) ||
+          ))) 
+        */
+        {
+
+          delete current[key];
+        }
+      });
+      // remove any leftover undefined values from the delete 
+      // operation on an array
+      if (_.isArray(current)) _.pull(current, undefined);
+
+      return current;
+
+    }(_.cloneDeep(obj));  // Do not modify the original object, create a clone instead
+  }
   actualizar(){
     console.log("Actualizando con ", this.state.correo );
-    let dato = JSON.parse(this.state.campo);
+    let dato = this.pruneEmpty(JSON.parse(this.state.campo));
     console.log(dato);
     dato["id_user"] = this.state.correo;
     FileService.postCampo('5661b2a2f3b30194336dc7a6', 'full_description', dato).then(data => {
       console.log("El resultado del post es: ", data);
+      console.log("El dato del get es: ", data);
+      alert(data.message+" - Versión: "+data.version);
     }).catch(err => {
       console.log(err);
     })
